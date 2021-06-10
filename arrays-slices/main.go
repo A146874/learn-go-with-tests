5216a5b0f7adeff43b2443594d1aba72f58e6dfa
a146874 <christopher_defreitas@progressive.com>
2021-06-10 07:51:42 -0400

    Sums of multipls slices

--- arrays-slices/main.go
+++ arrays-slices/main.go
@@ -7,3 +7,12 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return
+}
+
+// BOOKMARK: https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/arrays-and-slices#write-the-test-first-3
--- arrays-slices/sum_test.go
+++ arrays-slices/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	numbers := []int{1, 2, 3, 4, 5}
@@ -10,5 +13,14 @@ func TestSum(t *testing.T) {
 
 	if got != want {
 		t.Errorf("got %d want %d given %v", got, want, numbers)
+	}
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2, 3}, []int{1, 2, 3, 4})
+	want := []int{6, 10}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %d want %d", got, want)
 	}
 }
